903896339585626cc448ff44ba2e6113e3800cf2
Juan Luis Canedo <juan.canedo.v@ucb.edu.bo>
2020-12-01 09:46:03 -0400

    Ya funciona los descuentos

--- lib/Precio.rb
+++ lib/Precio.rb
@@ -1,7 +1,31 @@
 class Precio
     def calcular_precio_final(cantidad_items=0,precio_unitario=0,estado=' ')
-        return 10
+            descuento_cantidad=devolverDescuento(cantidad_items)
+            return cantidad_items*precio_unitario-(cantidad_items*precio_unitario*descuento_cantidad)/100
+        
+
     end
+
+    def devolverDescuento(num_compras)
+        respuesta=0
+        if(num_compras==1000)
+            respuesta=0.03
+        end
+        if(num_compras==3000)
+            respuesta=0.05
+        end
+        if(num_compras==7000)
+            respuesta=0.07
+        end
+        if(num_compras==10000)
+            respuesta=0.1
+        end
+        if(num_compras==30000)
+            respuesta=0.15
+        end
+        return respuesta
+    end
+
 
 
 
